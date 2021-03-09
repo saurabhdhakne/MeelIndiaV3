595f22e37fa0c60e26e845cf5adf83a2622f0ae3
saurabhdhakne <sbdhakne309@gmail.com>
2021-03-09 09:08:51 +0530

    domain added to redirect

--- passport-setup.js
+++ passport-setup.js
@@ -13,7 +13,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: "416605011673-84me0ll76ug0orh56l6ke9foc3f3n46i.apps.googleusercontent.com",
     clientSecret: "CiMh_S62F3PxRy_bzqm26GfF",
-    callbackURL: "https://localhost/google/callback"
+    callbackURL: "https://meelindia/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
       return cb(null, profile);
